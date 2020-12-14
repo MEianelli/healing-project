6dd7cf081f828a935acdfcf9f52aed7c40c4fbf0
MEianelli <filipelouzano@gmail.com>
2020-12-14 18:13:53 -0300

    Primeiro dia, comecei a montar os containers

---
+++ data.js
@@ -0,0 +1,94 @@
+export const data = {
+  boss: {
+    mainAttack: 'normal-melee',
+    fightDuration: 90,
+    spells: ['aoe', 'cleave', 'ultimate', 'burst', 'heal'],
+  },
+  healthBars: [
+    {
+      type: 'tank',
+      maxHealth: 10000,
+      currentHealth: 10000,
+      currentAura: 'none',
+    },
+    {
+      type: 'melee',
+      maxHealth: 7000,
+      currentHealth: 7000,
+      currentAura: 'none',
+    },
+    {
+      type: 'melee',
+      maxHealth: 7000,
+      currentHealth: 7000,
+      currentAura: 'none',
+    },
+    {
+      type: 'melee',
+      maxHealth: 7000,
+      currentHealth: 7000,
+      currentAura: 'none',
+    },
+    {
+      type: 'range',
+      maxHealth: 4000,
+      currentHealth: 4000,
+      currentAura: 'none',
+    },
+    {
+      type: 'range',
+      maxHealth: 4000,
+      currentHealth: 4000,
+      currentAura: 'none',
+    },
+    {
+      type: 'range',
+      maxHealth: 4000,
+      currentHealth: 4000,
+      currentAura: 'none',
+    },
+    {
+      type: 'range',
+      maxHealth: 4000,
+      currentHealth: 4000,
+      currentAura: 'none',
+    },
+    {
+      type: 'range',
+      maxHealth: 4000,
+      currentHealth: 4000,
+      currentAura: 'none',
+    },
+    {
+      type: 'range',
+      maxHealth: 4000,
+      currentHealth: 4000,
+      currentAura: 'none',
+    },
+    {
+      type: 'range',
+      maxHealth: 4000,
+      currentHealth: 4000,
+      currentAura: 'none',
+    },
+    {
+      type: 'range',
+      maxHealth: 4000,
+      currentHealth: 4000,
+      currentAura: 'none',
+    },
+  ],
+  player: {
+    manaBar: {
+      currentManaPots: 0,
+      maxMana: 10000,
+      currentMana: 10000,
+    },
+    skills: {
+      tap: 'renew',
+      cast: 'flash',
+      aoe: 'circle',
+      ultimate: 'allrenew',
+    },
+  },
+};
--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 /// <reference path="./p5.global-mode.d.ts" />;
-window.onload = function () {};
+import { data } from './data.js';
+const { boss, healthBars, player } = data;
+
+const bossSpellList = document.querySelector('.spell-list');
+const bossSpellsColors = ['teal', 'sandybrown', 'slateblue', 'paleturquoise', 'rgb(133, 37, 2)'];
+
+window.onload = function () {
+  createBossSpellList(boss);
+};
+
+function createBossSpellList({ spells }) {
+  spells.forEach(element => {
+    const newSpell = document.createElement('li');
+    const spellIcon = document.createElement('div');
+    const spellText = document.createElement('div');
+
+    spellIcon.className = 'boss-spell-icon';
+    spellText.className = 'boss-spell-text';
+
+    spellIcon.style.backgroundColor = bossSpellsColors.shift();
+    spellText.innerHTML = element;
+
+    newSpell.appendChild(spellIcon);
+    newSpell.appendChild(spellText);
+
+    newSpell.className = 'boss-spell';
+
+    bossSpellList.appendChild(newSpell);
+  });
+}
